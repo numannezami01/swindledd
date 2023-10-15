a66bd886b576778ad350d509da63b7f040270184
numannezami01 <numannezami217@gmail.com>
2023-10-15 16:52:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// var cur = document.querySelector("#cursor")
+//  document.addEventListener("mousemove",function(dets){
+//   cur.style.left =`${dets.x}px`
+//   cur.style.top =`${dets.y}px`
+// })
+  
+
+
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+    
+
+}
+loco();
+
+  
+  
+gsap.from(".product6",{
+  opacity:"0",
+delay:1,
+  stagger:3,
+  repeat:-3,
+
+})
+ 
